d83dc89884592a4e1e1df610010be9717a6ba3c1
Wayne <22000210@myrp.edu.sg>
2023-08-10 18:25:58 +0800

    Completed

--- app/src/main/java/sg/edu/rp/c346/id22000210/l12demodialog/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000210/l12demodialog/MainActivity.java
@@ -1,14 +1,46 @@
 package sg.edu.rp.c346.id22000210.l12demodialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDemo1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDemo1 = findViewById(R.id.buttonDemo1);
+
+        btnDemo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Create the Dialog Builder
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                // Set the dialog details
+                /*
+                myBuilder.setTitle("Demo 1-Simple Dialog");
+                myBuilder.setMessage("I can develop Android App.");
+                myBuilder.setCancelable(false);
+                myBuilder.setPositiveButton("Close", null);
+                 */
+
+                myBuilder.setTitle("Congratulations");
+                myBuilder.setMessage("You have completed a simple Dialog Box");
+                myBuilder.setCancelable(false);
+                myBuilder.setPositiveButton("Dismiss", null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
     }
 }
